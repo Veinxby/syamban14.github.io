8bb1f3fdc5ac2bbe0adc68106df46f8c53f132a0
syamban14 <syamsulikj.48@gmail.com>
2023-08-15 23:08:11 +0700

    melakukan perubahan di beberapa file

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,23 @@
 
 namespace App\Controllers;
 
+use App\Models\Perpustakaan;
+
 class Home extends BaseController
 {
+
+    protected $dataBuku;
+
+    public function __construct()
+    {
+        $this->dataBuku = new Perpustakaan();
+    }
+
     public function index(): string
     {
-        return view('home/index');
+        $data = [
+            'buku' => $this->dataBuku->getDataBuku()
+        ];
+        return view('home/index', $data);
     }
 }
